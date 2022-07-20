ba35d6cc14a7d358fba7edef80906b5296c9afc5
UnnamedOrange <lycheng1215@sina.com>
2022-07-20 10:11:24 +0800

    Design the layout

--- Consciousness/i18n/Consciousness_zh_CN.ts
+++ Consciousness/i18n/Consciousness_zh_CN.ts
@@ -4,16 +4,34 @@
 <context>
     <name>main_window</name>
     <message>
-        <location filename="../window/main_window.ui" line="28"/>
-        <location filename="../window/main_window.ui" line="28"/>
-        <source>&amp;Program</source>
-        <translation type="unfinished">程序(&amp;P)</translation>
+        <location filename="../window/main_window.ui" line="23"/>
+        <location filename="../window/main_window.ui" line="23"/>
+        <source>Windows to Monitor</source>
+        <translation>监视的窗口</translation>
     </message>
     <message>
-        <location filename="../window/main_window.ui" line="36"/>
-        <location filename="../window/main_window.ui" line="36"/>
+        <location filename="../window/main_window.ui" line="65"/>
+        <location filename="../window/main_window.ui" line="65"/>
+        <source>Window Name</source>
+        <translation>窗口名</translation>
+    </message>
+    <message>
+        <location filename="../window/main_window.ui" line="72"/>
+        <location filename="../window/main_window.ui" line="72"/>
+        <source>Window Class Name</source>
+        <translation>窗口类名</translation>
+    </message>
+    <message>
+        <location filename="../window/main_window.ui" line="113"/>
+        <location filename="../window/main_window.ui" line="113"/>
+        <source>&amp;Program</source>
+        <translation>程序(&amp;P)</translation>
+    </message>
+    <message>
+        <location filename="../window/main_window.ui" line="121"/>
+        <location filename="../window/main_window.ui" line="121"/>
         <source>&amp;Exit</source>
-        <translation type="unfinished">退出(&amp;E)</translation>
+        <translation>退出(&amp;E)</translation>
     </message>
 </context>
 </TS>
