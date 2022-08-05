733eb0548e8ac7d0845f70101950130ec3c43784
UnnamedOrange <lycheng1215@sina.com>
2022-08-05 10:30:00 +0800

    Integrate tray and main window

--- Consciousness/main.cpp
+++ Consciousness/main.cpp
@@ -7,17 +7,36 @@
  * See the LICENSE file in the repository root for full license text.
  */
 
-#include <window/main_window.h>
+#include <memory>
 
 #include <QApplication>
 
 #include <utils/i18n.hpp>
+#include <window/main_window.h>
+#include <window/tray.h>
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
     utils::i18n::change_language_to_system_default();
-    main_window w;
-    w.show();
+
+    tray tray;
+    tray.show_system_tray();
+
+    std::shared_ptr<main_window> w;
+    tray.set_on_show_main_window([&]() {
+        if (!w || !w->isVisible())
+        {
+            w = std::make_shared<main_window>(tray.get_config_store());
+            w->setAttribute(Qt::WA_QuitOnClose, false);
+            w->show();
+            w->raise();
+            w->activateWindow();
+        }
+        else
+        {
+            w.reset();
+        }
+    });
     return a.exec();
 }
