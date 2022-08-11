89a30fdf517a5ed08afe0fb06d562de2acd16acf
UnnamedOrange <lycheng1215@sina.com>
2022-08-11 11:20:55 +0800

    Create record_status.h

---
+++ Consciousness/core/record_status.h
@@ -0,0 +1,17 @@
+/**
+ * @file record_status.h
+ * @author UnnamedOrange
+ * @brief Extra statuses for a record.
+ *
+ * @copyright Copyright (c) UnnamedOrange. Licensed under the MIT License.
+ * See the LICENSE file in the repository root for full license text.
+ */
+
+#pragma once
+
+#include "record.h"
+
+namespace consciousness
+{
+    
+}
