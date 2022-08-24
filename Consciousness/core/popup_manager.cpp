7e4c81ee8204709d4d1c91e8a7299a40499b659c
UnnamedOrange <lycheng1215@sina.com>
2022-08-24 19:51:35 +0800

    Implement popup manager

---
+++ Consciousness/core/popup_manager.cpp
@@ -0,0 +1,41 @@
+/**
+ * @file popup_manager.cpp
+ * @author UnnamedOrange
+ * @brief Manage popup dialogs using slots and signals.
+ *
+ * @copyright Copyright (c) UnnamedOrange. Licensed under the MIT License.
+ * See the LICENSE file in the repository root for full license text.
+ */
+
+#include "core.h"
+
+#include "popup_manager.h"
+
+#include <QObject>
+
+#include <window/popup.h>
+
+using namespace consciousness;
+
+popup_manager::popup_manager(core& _core) : _core(_core)
+{
+    connect(this, &popup_manager::create_popup_signal, this,
+            &popup_manager::on_create_popup);
+}
+
+void popup_manager::create_popup(uint32_t id, const std::u16string& alias)
+{
+    emit create_popup_signal(id, alias);
+}
+
+void popup_manager::on_create_popup(uint32_t id, const std::u16string& alias)
+{
+    popup* p = new popup(id, QString::fromStdU16String(alias));
+    p->setAttribute(Qt::WA_DeleteOnClose);
+    p->open();
+}
+
+void popup_manager::on_popup_closed(uint32_t id, bool permit, int minutes)
+{
+    _core.notify_popup_closed({id, permit, minutes});
+}
---
+++ Consciousness/core/popup_manager.h
@@ -0,0 +1,61 @@
+/**
+ * @file popup_manager.h
+ * @author UnnamedOrange
+ * @brief Manage popup dialogs using slots and signals.
+ *
+ * @copyright Copyright (c) UnnamedOrange. Licensed under the MIT License.
+ * See the LICENSE file in the repository root for full license text.
+ */
+
+#pragma once
+
+#include <string>
+
+#include <QObject>
+
+namespace consciousness
+{
+    class core;
+
+    struct popup_close_t
+    {
+        uint32_t id;
+        bool permit;
+        int minutes;
+    };
+
+    class popup_manager : public QObject
+    {
+        Q_OBJECT
+
+    private:
+        core& _core;
+
+    public:
+        popup_manager(core& _core);
+
+    public:
+        /**
+         * @brief Call from any thread to create a popup.
+         *
+         * @param id ID of the record.
+         * @param alias The alias to show on the popup.
+         */
+        void create_popup(uint32_t id, const std::u16string& alias);
+
+    private slots:
+        /**
+         * @brief Create a popup on main thread.
+         */
+        void on_create_popup(uint32_t id, const std::u16string& alias);
+
+    signals:
+        void create_popup_signal(uint32_t id, const std::u16string& alias);
+
+    public slots:
+        /**
+         * @brief Call from main thread to that the popup has closed.
+         */
+        void on_popup_closed(uint32_t id, bool permit, int minutes);
+    };
+} // namespace consciousness
