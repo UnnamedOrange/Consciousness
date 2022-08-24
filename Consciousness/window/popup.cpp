c981b33e97e3112aceb46a1e8fa9f74514bd0d2f
UnnamedOrange <lycheng1215@sina.com>
2022-08-24 16:46:03 +0800

    Fix resizing on language change

--- Consciousness/window/popup.cpp
+++ Consciousness/window/popup.cpp
@@ -9,11 +9,13 @@
 
 #include "popup.h"
 
+#include <QLayout>
+
 popup::popup(QWidget* parent) : QDialog(parent)
 {
     ui.setupUi(this);
     setAttribute(Qt::WidgetAttribute::WA_QuitOnClose, false);
-    setFixedSize(minimumSize());
+    ui.verticalLayout->setSizeConstraint(QLayout::SetFixedSize);
     setWindowFlags(windowFlags() | Qt::MSWindowsFixedSizeDialogHint |
                    Qt::WindowStaysOnTopHint);
 }
@@ -22,9 +24,5 @@ void popup::changeEvent(QEvent* event)
 {
     QDialog::changeEvent(event);
     if (event->type() == QEvent::LanguageChange)
-    {
         ui.retranslateUi(this);
-        // TODO: Use a scheduler to resize.
-        setFixedSize(minimumSize());
-    }
 }
